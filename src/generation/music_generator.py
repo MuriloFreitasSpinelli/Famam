9dad59696939dbadc063af7c7960c8a56b394f21
urzxz <urzxz@student.kit.edu>
2026-01-14 12:19:56 +0100

    Added a parse data method in the music_generator.py.

--- src/generation/music_generator.py
+++ src/generation/music_generator.py
@@ -1,6 +1,22 @@
 import tensorflow as tf
+from typing import List
+
+from src.data.dataset_vocabulary import DatasetVocabulary
 
 
-def generate_music_from_data(savemodel, data):
+def generate_music_from_data(savedmodel, data):
+    outputs = []
+    for sample in data:
+        prediction = savedmodel(sample, training=False)
+        outputs.append(prediction)
+    return tf.concat(outputs, axis=0)
+
+
+def parse_data(data : List[str], library : DatasetVocabulary):
+    genre = data[0]
+    genre_id = library.get_genre_id(genre)
+    # if (library.genre_to_id)
+
     pass
+        #       First item of the list is the genre
 
